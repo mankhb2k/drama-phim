b216d05dde14107b25372d2339960654546f2d02
mankhb2k <mankhb2k@gmail.com>
2026-02-03 16:29:56 +0700

    first commit

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, Role } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = "admin@drama.app";
+  const password = "admin123";
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const admin = await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      email,
+      name: "Super Admin",
+      password: hashedPassword,
+      role: Role.ADMIN,
+    },
+  });
+
+  console.log("✅ Admin user created:", admin.email);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
